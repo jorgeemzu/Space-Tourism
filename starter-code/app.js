9e6ae53de2a8385d914af4d86f4e9eb1b0261b48
jorge <jorge.emzu@gmail.com>
2021-12-31 18:16:59 -0400

    se finalizon con la pagina de Home en todas sus versiones

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,24 +1,25 @@
-const navToggle = document.getElementById('nav-toggle')
+const navToggle = document.querySelectorAll('.nav-toggle')
 const nav = document.getElementById('primary-navigation')
 
-navToggle.addEventListener('click', () =>{
-    const prueba = nav.getAttribute('data-visible')
-    
-    
-    console.log(prueba)
-    if(prueba === 'false'){
-        
-        nav.className = 'primary-navigation primary-navigation-expanded flex'
-        nav.setAttribute('data-visible', true)
-    }else{
-        
-        nav.className = ' primary-navigation  flex';
-        nav.setAttribute('data-visible', false)
-        
-    }
-    
-    
 
+navToggle.forEach( (toggle) =>{
+    toggle.addEventListener('click', () =>{
+        const prueba = nav.getAttribute('data-visible')
+        if(prueba === 'false'){
+            
+            nav.className = 'primary-navigation primary-navigation-expanded flex'
+            nav.setAttribute('data-visible', true)
+        }else{
+            
+            nav.className = ' primary-navigation  flex';
+            nav.setAttribute('data-visible', false)
+            
+        }
+        
+        
+    
+    })
 })
+
 
 
